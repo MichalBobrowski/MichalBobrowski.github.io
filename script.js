a8abf8224500cca24e0fc7a943a46c7e992d8d73
“Michal <bobrowski92@gmail.com>
2017-12-03 11:58:33 +0100

    Widok x6

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://secret-castle-54042/v1/task/';
+  const apiRoot = 'https://secret-castle-54042.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://secret-castle-54042.herokuapp.com/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
